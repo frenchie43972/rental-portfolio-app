54045634171505a575754cba0b18e9eecd01a483
Kris French <frenchie43972@gmail.com>
2024-04-11 13:49:57 +0900

    starting backend

---
+++ rental-property-portfolio/config/database.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+
+let connected = false;
+
+const connectDB = async () => {
+  mongoose.set('strictQuery', true);
+
+  // If the DB is already connected, do not connect again
+  if (connected) {
+    console.log('MongoDB already connected...');
+    return;
+  }
+
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    connected = true;
+    console.log('MongoDB connected...');
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export default connectDB;
