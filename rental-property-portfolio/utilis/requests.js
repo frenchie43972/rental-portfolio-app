49d7f9667739bd2c1f1a36122daa712926088568
Kris French <frenchie43972@gmail.com>
2024-04-17 14:38:11 +0900

    building properties

--- rental-property-portfolio/utilis/requests.js
+++ rental-property-portfolio/utilis/requests.js
@@ -1,5 +1,6 @@
 const apiDomain = process.env.NEXT_PUBLIC_API_DOMAIN || null;
 
+// Fetch all properties
 async function fetchProperties() {
   try {
     // Handle the case where the domin is not avaia ble yet
@@ -18,4 +19,23 @@ async function fetchProperties() {
   }
 }
 
-export {fetchProperties}; 
+// Fetch single property
+async function fetchProperty(id) {
+  try {
+    // Handle the case where the domin is not avaiable yet
+    if (!apiDomain) {
+      return null;
+    }
+    const response = await fetch(`${apiDomain}/properties/${id}`);
+
+    if (!response.ok) {
+      throw new Error('Failed to fetch data.');
+    }
+    return response.json();
+  } catch (error) {
+    console.log(error)
+    return null;
+  }
+}
+
+export {fetchProperties, fetchProperty}; 
